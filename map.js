b4558d6133e3dc3cfb0b91f54996c69502177d67
BrianDoce <docena.brian@gmail.com>
2025-02-19 17:04:29 -0800

    Finished 3.1

--- map.js
+++ map.js
@@ -1,12 +1,57 @@
-   // Set your Mapbox access token here
-   mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpYW4tZG9jIiwiYSI6ImNtN2NsNzd6ZTB0dmgybHB3aWdrNzhlNDQifQ.bi4s839wOtmNosqOlDm0aA';
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpYW4tZG9jIiwiYSI6ImNtN2NsNzd6ZTB0dmgybHB3aWdrNzhlNDQifQ.bi4s839wOtmNosqOlDm0aA';
 
-   // Initialize the map
-   const map = new mapboxgl.Map({
+// Initialize the map
+const map = new mapboxgl.Map({
      container: 'map', // ID of the div where the map will render
      style: 'mapbox://styles/mapbox/streets-v12', // Map style
      center: [-71.09415, 42.36027], // [longitude, latitude]
      zoom: 12, // Initial zoom level
      minZoom: 5, // Minimum allowed zoom
      maxZoom: 18 // Maximum allowed zoom
-   });
+});
+
+map.on('load', () => { 
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+    });
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 5,          // Thicker lines
+            'line-opacity': 0.6       // Slightly less transparent
+        }
+    });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 5,          // Thicker lines
+            'line-opacity': 0.6       // Slightly less transparent
+        }
+    });
+});
+
+map.on('load', () => {
+    // Load the nested JSON file
+    const jsonurl = INPUT_BLUEBIKES_CSV_URL
+    d3.json(jsonurl).then(jsonData => {
+        console.log('Loaded JSON Data:', jsonData);  // Log to verify structure
+        const stations = jsonData.data.stations;
+        console.log('Stations Array:', stations);
+    }).catch(error => {
+      console.error('Error loading JSON:', error);  // Handle errors if JSON loading fails
+    });
+});
